d348b25fbeeea8d9857e8ccfd2182fe87281e4a5
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-10 17:28:59 +0100

    Updated and refactored DockingStation class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,28 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    #Bike.new
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
   end
 
   def dock bike
-  	fail 'Docking station full' if @bike
-    @bike = bike
+  	fail 'Docking station full' if full?
+    @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 
