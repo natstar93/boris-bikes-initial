6a54a064ab021f0cc76e672240f7e687a45eac88
Natalie Akam <natalie.akam@gmail.com>
2015-06-12 14:17:57 +0100

    Added default capacity in response to test

--- lib/van.rb
+++ lib/van.rb
@@ -1,5 +1,13 @@
-class Van
-  def take_bike bike
+require_relative 'bike'
 
+class Van
+  DEFAULT_CAPACITY = 10
+
+  def take_bike bike
+  	@bike = bike
+  end
+
+  def release_bike
+  	@bike
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,6 +3,18 @@ require 'van'
 describe Van do 
 
   it { is_expected.to respond_to(:take_bike).with(1).argument }
+  
+  # it 'takes a bike' do
+  #   expect(subject.take_bike :bike).to 
+  # end
 
-	
+  it 'releases a bike' do
+  	subject.take_bike :bike
+  	expect(subject.release_bike).to be :bike
+  end
+
+  it 'has a default capacity of 10' do
+    expect(Van::DEFAULT_CAPACITY).to eq 10
+  end
+
 end
